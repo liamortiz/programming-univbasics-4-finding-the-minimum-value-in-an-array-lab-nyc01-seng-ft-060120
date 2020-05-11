d93240fcc9df81e8aa480f49e4c56797aa062078
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 16:31:43 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
 def find_min_value(array)
-  # Add your solution here
+  min_value = array[0]
+  
+  array.size.times do |index|
+    if array[index] < min_value
+      min_value = array[index]
+    end
+  end
+  
+  return min_value
 end
